530649fdd4a95fa51e4b420b5db278d0b3e9d6f5
Benian44 <155669009+Benian44@users.noreply.github.com>
2025-05-01 14:52:07 +0000

    Add files via upload

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -2,12 +2,10 @@ import React, { useState } from 'react';
 
 // Placeholder client logos
 const clients = [
-  { name: 'Client 1', logo: 'https://via.placeholder.com/150x80?text=Client+1' },
-  { name: 'Client 2', logo: 'https://via.placeholder.com/150x80?text=Client+2' },
-  { name: 'Client 3', logo: 'https://via.placeholder.com/150x80?text=Client+3' },
-  { name: 'Client 4', logo: 'https://via.placeholder.com/150x80?text=Client+4' },
-  { name: 'Client 5', logo: 'https://via.placeholder.com/150x80?text=Client+5' },
-  { name: 'Client 6', logo: 'https://via.placeholder.com/150x80?text=Client+6' },
+  { name: 'Client 1', logo: 'https://z-p3-scontent.fabj4-2.fna.fbcdn.net/v/t39.30808-6/412994336_122116966052137630_2822837578760795290_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeH9LY4F2RbQg4OYXDU1VQxm7vgrh7sy_iHu-CuHuzL-IRF3Si25qSWTEDYGdOOdS9M7QFu0ZMjUlVwqGSZHytkk&_nc_ohc=mmNlrHMrzacQ7kNvwG7jUUo&_nc_oc=AdmY_njVIK49Dkp5jyGLWSLJaEcneY4gSaS4IPWilua_-yNklILGx0GccJFs143dCxQ&_nc_zt=23&_nc_ht=z-p3-scontent.fabj4-2.fna&_nc_gid=hfORFu2eDy1FPoYLqavB8w&oh=00_AfF_2SFl26uDzCQaK1-8lHpxf0ybiI1CTUx-oi_SP39L4w&oe=68194AA5' },
+  { name: 'Client 2', logo: 'https://z-p3-scontent.fabj4-2.fna.fbcdn.net/v/t39.30808-6/489892236_122151501716559515_8250571763800715958_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeEtYnTo6iYsVCINFcDBeMESj7-yHBX0brqPv7IcFfRuuvgsEnQfLAtkdXwxBcHk51X5Gq2AfJf1RhNMcen41hiW&_nc_ohc=YrOn-vqRZMUQ7kNvwFvRklj&_nc_oc=AdlBn2Q5ZAXa5svLMaJw2qfcgfiQcCldpJI0sCaN1hIVtVaOzS6gp9Xc2QuF8IMhVas&_nc_zt=23&_nc_ht=z-p3-scontent.fabj4-2.fna&_nc_gid=-td62lYHP4kdgZRFAapi3A&oh=00_AfFtM_26n0gWHuyuMbAI21Qzv4iAGxXjHBazu_HmoFiCpQ&oe=6819520B' },
+  { name: 'Client 3', logo: 'https://z-p3-scontent.fabj4-2.fna.fbcdn.net/v/t39.30808-6/459397616_920385703442274_4945859248843589870_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHzUZlJ2ah3QlrWOlVNN0KlOvmfaiieFGg6-Z9qKJ4UaBkwNO6o8Et7UJpKTU9tgb8OBDDDSi1QNblgjyBGLGPW&_nc_ohc=8MdSowHAC2sQ7kNvwGti5hl&_nc_oc=Adk7oq0m8PMb7NqKmFokuzIVXlDVmZhjOLVrbv7QST7x8D59Ce6JAboq8SOURP4mkzw&_nc_zt=23&_nc_ht=z-p3-scontent.fabj4-2.fna&_nc_gid=LkrxNGxlNJCzlJTAiScPng&oh=00_AfHzuTw8efI9TfrQr_Ndka4JMwk_N8sAuVH0Kf40QvZSYg&oe=68196581' },
+  { name: 'Client 4', logo: 'https://z-p3-scontent.fabj4-2.fna.fbcdn.net/v/t39.30808-6/461834195_122160069404250173_5059105853948447865_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHyOV6d_3-dBRlcAQA7QidEEH0jSwcwHpgQfSNLBzAemMI2k0i9Tj1z9uu9G0OIxTK4Q0IiIUYfyvFMsnHeDNtc&_nc_ohc=oCtPeqHEod0Q7kNvwGh6wGm&_nc_oc=Adk64RIBCkKfxw3lWvzUVQcxwvUfu-18gDKofU8EM-9Q4_-cMjkiBAWh8EmSItq0gd0&_nc_zt=23&_nc_ht=z-p3-scontent.fabj4-2.fna&_nc_gid=Eopqsj-X4ZtDQ7gkZIXr-w&oh=00_AfFedku8u7gM_xsSPeWr3GhzUT0kLNlhXZdM0lOtKswKKw&oe=68194DA9' },
 ];
 
 // Portfolio projects
